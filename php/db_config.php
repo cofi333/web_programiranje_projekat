06203d7d55a163c7fddfa688051a74657a91d50a
Stefan <stefanbvts@gmail.com>
2023-06-23 20:30:18 +0200

    db_config

--- php/db_config.php
+++ php/db_config.php
@@ -1,8 +1,35 @@
 <?php
+//const HOST = "localhost";
+//const USER = "programatori";
+//const PASSWORD = "4sqwbd4SfhJhvCL";
+//const DB = "programatori";
+//const CHARSET = "utf8mb4";
+
 const HOST = "localhost";
-const USER = "programatori";
-const PASSWORD = "4sqwbd4SfhJhvCL";
+const USER = "root";
+const PASSWORD = "";
 const DB = "programatori";
+const CHARSET = "utf8mb4";
 
-$connection = mysqli_connect(HOST,USER,PASSWORD,DB);
+$pdoOptions = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
 
+
+$dsn = "mysql:host=" . HOST  . ";dbname=" . DB . ";charset=". CHARSET;
+
+function connectDatabase(string $dsn, array $pdoOptions): PDO
+{
+
+    try {
+        $pdo = new PDO($dsn, USER, PASSWORD , $pdoOptions);
+
+    } catch (\PDOException $e) {
+        var_dump($e->getCode());
+        throw new \PDOException($e->getMessage());
+    }
+
+    return $pdo;
+}
