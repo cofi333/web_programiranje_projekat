ac286b5979ce96711a91bcef9e23484c980bd837
Stefan <stefanbvts@gmail.com>
2023-06-25 16:28:19 +0200

    fetchData

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+    <title>Events</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -58,13 +58,14 @@
                     echo '<li class="nav-item create_event_btn">';
                     echo '<a class="nav-link" href="./event.php">Create an event</a>';
                     echo '</li>';
+                    echo '</ul>';
 
                     echo '<div class="user">';
                     echo '<a href="user_profile.php"><img src="./images/user_image.png" id="user_image"></a>';
                     echo '</div>';
                 }
                 ?>
-            </ul>
+
            
           </div>
         </div>
