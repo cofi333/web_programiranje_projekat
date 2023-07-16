de1117b8909ce54df5d9d7659977b276ca24595b
Stefan <stefanbvts@gmail.com>
2023-07-16 22:00:57 +0200

    user-profile

--- user_profile.php
+++ user_profile.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="node_modules/swiper/swiper-bundle.min.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./sass/style.css">
     <title>Your Profile</title>
 </head>
 <body>
