6905c5984840fb5e9cd63f153b06a489d11d539d
Stefan <stefanbvts@gmail.com>
2023-07-18 21:08:08 +0200

    ban user changes revert

--- admin/php/ban-user.php
+++ admin/php/ban-user.php
@@ -1,12 +1,9 @@
 <?php
     session_start();
     require_once '../../php/config.php';
-    $selectedUser = '';
 
 
  if(isset($_GET['id_user'])) {
-     $selectedUser = $_GET['id_user'];
-     if(isset($_SESSION['id_user']) and ($_SESSION['id_user'] === $selectedUser)){
          try{
              $sql = ("UPDATE users SET is_banned = 1 WHERE id_user = " . $_GET['id_user'] . " AND is_banned = 0");
              $stmt = $pdo->prepare($sql);
@@ -17,11 +14,6 @@
              throw new \PDOException($e->getMessage());
          }
          unset($_SESSION["id_user"]);
-     } else{
-         echo 'something wrong with if';
-         var_dump('session id -> ' . $_SESSION['id_user']);
-         var_dump('<br/>' . 'get id -> ' . $selectedUser);
-     }
- } else {
+     } else {
      redirection("../admin.php?m=26");
  }
